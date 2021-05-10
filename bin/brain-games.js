413af0873b6623184b4465a65da05d349d43a409
Anneta <nikolaevaanneta@gmail.com>
2021-05-10 20:56:36 +0200

    install CodeClimate and Eslint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
 
+import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!');
 console.log('May I have your name? ');
 const name = readlineSync.question('Your answer: ');
-console.log('Hello, ' + name + '!');
+console.log(`Hello, ${name}!`);
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
 import readlineSync from 'readline-sync';
 
-
 console.log('Welcome to the Brain Games!');
 console.log('May I have your name? ');
 const name = readlineSync.question('Your answer: ');
-console.log('Hello, ' + name + '!');
+console.log(`Hello, ${name}!`);
