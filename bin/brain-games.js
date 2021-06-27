e250cc9125f8f55dea01bf1bf6499d7f5419ff0e
Anneta <nikolaevaanneta@gmail.com>
2021-06-27 15:36:33 +0500

    the brain-calc is completed

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 
 import readlineSync from 'readline-sync';
+import calculator from './brain-calc.js';
 import brainEven from './brain-even.js';
+import gcd from './brain-gcd.js';
 
 console.log('Welcome to the Brain Games!');
 console.log('May I have your name? ');
 export const name = readlineSync.question('');
 console.log(`Hello, ${name}!`);
 
-brainEven();
+//brainEven();
+calculator();
+//gcd();
